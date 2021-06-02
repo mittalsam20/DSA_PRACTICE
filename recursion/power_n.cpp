afac963e99391f7d0eacd8f599cc0437792c3cae
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-02 19:50:28 +0530

    backtracking

---
+++ recursion/power_n.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int power(int n, int p)
+{
+    if (p == 0)
+        return 1;
+    int ans = power(n, p - 1);
+    return n * ans;
+}
+int main()
+{
+
+    int n, p;
+    cout << "Number:-";
+    cin >> n;
+    cout << "Power:-";
+    cin >> p;
+    cout << power(n, p) << endl;
+}
