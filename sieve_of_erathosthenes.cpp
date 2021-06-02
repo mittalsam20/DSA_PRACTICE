d77591b40112a4b47c60051b2e9bd6ea930b943a
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-02 17:18:30 +0530

    prime algo

---
+++ sieve_of_erathosthenes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void prime(int n)
+{
+    int prime_no[100] = {0}; //0 means unmarked
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime_no[i] == 0)
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                prime_no[j] = 1; //1 means marked
+            }
+        }
+    }
+    s for (int i = 2; i < n; i++) if (prime_no[i] == 0)
+            cout
+        << i << " ";
+}
+
+int main()
+{
+    int n;
+    cout << "Number:-";
+    cin >> n;
+    prime(n);
+}
