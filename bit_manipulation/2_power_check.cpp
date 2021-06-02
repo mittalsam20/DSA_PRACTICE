0c8e43b1e906e4a82d437207fe6a8031cbef2ab0
SAMAKSH MITTAL <sam.mittal@gmail.com>
2021-06-02 13:13:24 +0530

    bit questions more

--- bit_manipulation/2_power_check.cpp
+++ bit_manipulation/2_power_check.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 using namespace std;
+bool power_of_2(int n)
+{
+    return (n && !(n & (n - 1)));
+}
 int main()
 {
+    int n;
+    cout << "Number:-";
+    cin >> n;
+    cout << power_of_2(n);
 }
