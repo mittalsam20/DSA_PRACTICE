97e82f34c2925c648f836839aaf68fb62cd5d655
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-02 19:37:36 +0530

    recursion basics completed

---
+++ sieve of erathosthenes/prime_factorization.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void prime_factor()
+{
+    int spf[100] = {0};
+    for (int i = 2; i < -n; i++)
+    {
+    }
+}
+int main()
+{
+    int n;
+    cout << "NUMBER:-";
+    cin >> n;
+    int a[] = { 1,
+                2,
+                3,
+                5,
+                7 } for (int i = 0; i < n; i++)
+    {
+    }
+}
