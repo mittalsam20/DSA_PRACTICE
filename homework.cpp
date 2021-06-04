8d76075f9d94d3a7cacfd47d63b35297a18c19cd
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-05 00:14:54 +0530

    done for the day

--- homework.cpp
+++ homework.cpp
@@ -4,11 +4,17 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, ans = 0, qt;
     cin >> n;
+    cin >> qt;
     int a[n], b[n];
     for (int i = 0; i < n; i++)
-    {
         cin >> a[i];
+    for (int i = 0; i < n; i++)
         cin >> b[i];
-    }
+
+    for (int i = 0; i < n; i++)
+        if (qt <= b[i])
+            ans++;
+    cout << ans << endl;
+}
--- matrix_diagonal_sum.cpp
+++ matrix_diagonal_sum.cpp
@@ -4,11 +4,26 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n, ans = 0;
     cin >> n;
-    int a[n], b[n];
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
-        cin >> b[i];
+        for (int j = 0; j < n; j++)
+        {
+            if (n % 2 && i == j && i == (n - 1) / 2)
+            {
+                ans += a[i][j];
+                continue;
+            }
+            else if (i == j)
+                ans += a[i][j];
+            else if (i + j == n - 1)
+                ans += a[i][j];
+        }
     }
+    cout << ans << endl;
+}
