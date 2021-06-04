84145679a6a28d9f70372078374ecc26e2ffda4f
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-04 23:51:14 +0530

    more array probs

---
+++ homework.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n], b[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        cin >> b[i];
+    }
