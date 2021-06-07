3b9af57ec9a73227a81ec507d607f684418eeabe
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-06-07 05:48:04 +0530

    1020

--- array/sum_of_subarray.cpp
+++ array/sum_of_subarray.cpp
@@ -8,7 +8,29 @@ https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
 */
 
 #include <iostream>
-using namepspace std;
+#include <climits>
+using namespace std;
+int sub_array_sum(int a[], int n);
+{
+    int mx = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += a[j];
+            mx = max(mx, sum);
+        }
+    }
+    return mx;
+}
+
 int main()
 {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sub_array_sum(a, n);
 }
