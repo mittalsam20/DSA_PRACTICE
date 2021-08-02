00ae54909106fe6418070914c3ac885b055982f8
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-02 23:49:03 +0530

    not counting number from now on

--- array_easy/max_sum_subarray.cpp
+++ array_easy/max_sum_subarray.cpp
@@ -1,18 +1,16 @@
-class Solution
+#include <iostream>
+using namespace std;
+int main()
 {
-public:
-    int maxSubArray(vector<int> &n)
+    int mx = n[0], t = 0;
+    for (int i = 0; i < n.size(); i++)
     {
-        int mx = n[0], t = 0;
-        for (int i = 0; i < n.size(); i++)
+        t += n[i];
+        mx = max(mx, t);
+        if (t < 0)
         {
-            t += n[i];
-            mx = max(mx, t);
-            if (t < 0)
-            {
-                t = 0;
-            }
+            t = 0;
         }
-        return mx;
     }
-};
+    return mx;
+}
