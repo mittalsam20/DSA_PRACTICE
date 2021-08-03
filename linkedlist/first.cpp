ffa072915abac00ece0f999aefe1a5c4a221a8d7
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-04 02:29:09 +0530

    deletiong from linked list

--- linkedlist/first.cpp
+++ linkedlist/first.cpp
@@ -62,6 +62,18 @@ bool search(node *head, int key)
     return false;
 }
 
+void deletion(node *&head, int val)
+{
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
 int main()
 {
     node *head = NULL;
@@ -71,7 +83,9 @@ int main()
     disp(head);
     insertAtHead(head, 4);
     disp(head);
-    cout << search(head, 2);
+    // cout << search(head, 2);
+    deletion(head, 2);
+    disp(head);
 
     return 0;
 }
