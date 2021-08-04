f4ad324294b92d9a2af155d659bde9fe7cdbb7d9
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-04 21:22:04 +0530

    done with detecting cylce in ll

---
+++ linkedlist/detectandremovecycle.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertathead(node *&head, int val)
+{
+
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void disp(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+void makecycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startnode;
+
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startnode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startnode;
+}
+
+bool detectcycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertathead(head, 4);
+    insertathead(head, 3);
+    insertathead(head, 2);
+    insertathead(head, 1);
+    insertathead(head, 9);
+    disp(head);
+    // makecycle(head, 2);
+    // disp(head);
+    cout << detectcycle(head);
+}
