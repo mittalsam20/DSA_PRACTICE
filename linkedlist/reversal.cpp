08bfa862d542f999ab980d112fd1422d5aad40c0
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-04 16:09:34 +0530

    reversal of ll

---
+++ linkedlist/reversal.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void disp(node *head)
+{
+    node *temp = head;
+    if (head == NULL)
+    {
+        cout << "Linked List is empty" << endl;
+        return;
+    }
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insert(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+node *rev(node *&head)
+{
+    node *prev = NULL;
+    node *current = head;
+    node *nextptr;
+    while (current != NULL)
+    {
+        nextptr = current->next;
+        current->next = prev;
+
+        prev = current;
+        current = nextptr;
+    }
+    head = prev;
+    return prev;
+}
+int main()
+{
+    node *head = NULL;
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 3);
+    insert(head, 4);
+    insert(head, 5);
+    disp(head);
+    node *newhead = rev(head);
+    disp(newhead);
+    disp(head);
+}
