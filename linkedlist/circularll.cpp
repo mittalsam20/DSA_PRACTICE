75a346a48ffd5ea6b885e438588f68d3d82fbe64
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-05 17:35:56 +0530

    cll

---
+++ linkedlist/circularll.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        n->next = n;
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = head;
+    head = n;
+    return;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        insertAtHead(head, val);
+        return;
+    }
+    node *temp = head;
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = head;
+}
+
+void disp(node *head)
+{
+    node *temp = head;
+    if (head == NULL)
+    {
+        cout << "Linked List is empty" << endl;
+        return;
+    }
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != head);
+    cout
+        << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtHead(head, 9);
+    insertAtHead(head, 8);
+    insertAtHead(head, 7);
+    disp(head);
+}
