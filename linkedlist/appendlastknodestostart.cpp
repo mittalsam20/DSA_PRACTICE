961776e4483c4d3bf621e91612a12a94fe30942c
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-05 10:53:44 +0530

    append k nodes from last of ll to first of ll

---
+++ linkedlist/appendlastknodestostart.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+void disp(node *head)
+{
+    node *temp = head;
+    if (head == NULL)
+    {
+        cout << "Linked List is empty" << endl;
+        return;
+    }
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+int length(node *head)
+{
+    int l = 0;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        l++;
+        temp = temp->next;
+    }
+    return l;
+}
+
+node *appendknodes(node *&head, int k)
+{
+    node *newhead;
+    node *newtail;
+    node *tail = head;
+    int l = length(head);
+    int count = 1;
+    while (tail->next != NULL)
+    {
+        if (count == l - k)
+        {
+            newtail = tail;
+        }
+        if (count == l - k + 1)
+        {
+            newhead = tail;
+        }
+        tail = tail->next;
+        count++;
+    }
+    newtail->next = NULL;
+    tail->next = head;
+    return newhead;
+}
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+    insertAtTail(head, 7);
+    disp(head);
+    node *newhead = appendknodes(head, 2);
+    disp(newhead);
+    return 0;
+}
